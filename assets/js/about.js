5b3576e5ec00aabe3f2d73b817f14d88e028a619
FrankN88 <nardecchia.f@gmail.com>
2021-05-28 16:26:16 +0000

    Email js working, to be styled and tested again

--- assets/js/about.js
+++ assets/js/about.js
@@ -1,11 +1,22 @@
-function sendMail (params) {
-    let tempParams = {
-        from_name : document.getElementById("fromName").value,
-        to_name : document.getElementById("toName").value,
-        message: document.getElementById("message").value,
-    };
-    emailjs.send("service_aa7yrwa", "template_em9dbwg", "tempParams")
-    .then(function(res){
-        console.log("success", res.status);
-    })
-}
+// EmailJS function //
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Sending...';
+
+   const serviceID = 'default_service';
+   const templateID = 'template_l5nl898';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Send Email';
+      alert('Sent!');
+    }, (err) => {
+      btn.value = 'Send Email';
+      alert(JSON.stringify(err));
+    });
+    this.reset()
+});
