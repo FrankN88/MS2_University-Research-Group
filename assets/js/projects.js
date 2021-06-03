d745c0dabed8c83f2bf529f4a225496fab822014
FrankN88 <nardecchia.f@gmail.com>
2021-06-03 16:16:19 +0000

    Creating var questions quiz

---
+++ assets/js/projects.js
@@ -0,0 +1,57 @@
+let quiz = {
+    // (A) PROPERTIES 
+    // (A1) QUESTIONS & ANSWERS
+    // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
+    data: [
+    {
+      q : "What is the approximated measure of the speed of light?",
+      o : [
+        "500 thousand meters/second",
+        "300 thousand meters/second",
+        "100 thousand meters/second",
+        "400 thousand meters/second"
+      ],
+      a : 1 
+    },
+    {
+      q : "How many elements there are in the periodic table of elements?",
+      o : [
+        "150",
+        "200",
+        "136",
+        "118"
+      ],
+      a : 3
+    },
+    {
+      q : "What is the formula of gravity of Earth?",
+      o : [
+        "g= 35 k s-2",
+        "g= 9.81 m m-2",
+        "g= 9.81 m s-2",
+        "g= 25 m s-2"
+      ],
+      a : 2
+    },
+    {
+      q : "Which is the seventh planet from the sun?",
+      o : [
+        "Uranus",
+        "Earth",
+        "Pluto",
+        "Mars"
+      ],
+      a : 0
+    },
+    {
+      q : "Which is the unit of measure of the magnetic field?",
+      o : [
+        "G",
+        "Watt",
+        "Candela",
+        "Gauss"
+      ],
+      a : 3
+    }
+]
+}
