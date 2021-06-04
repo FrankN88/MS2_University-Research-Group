dbea79e2c4d0a89f37ed342f9c01c33b213bf159
FrankN88 <nardecchia.f@gmail.com>
2021-06-04 16:00:10 +0000

    Adding new questions quiz

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -1,57 +1,56 @@
-let quiz = {
-    // (A) PROPERTIES 
-    // (A1) QUESTIONS & ANSWERS
-    // Q = QUESTION, O = OPTIONS, A = CORRECT ANSWER
-    data: [
+
+
+// Questions
+const questions = [
     {
-      q : "What is the approximated measure of the speed of light?",
-      o : [
-        "500 thousand meters/second",
-        "300 thousand meters/second",
-        "100 thousand meters/second",
-        "400 thousand meters/second"
-      ],
-      a : 1 
+        q:'What is the approximated measure of the speed of light?',
+        options:['300 thousand meters/second', '500 thousand meters/second', '200 thousand meters/second', '250 thousand meters/second'],
+        answer:0
     },
     {
-      q : "How many elements there are in the periodic table of elements?",
-      o : [
-        "150",
-        "200",
-        "136",
-        "118"
-      ],
-      a : 3
+        q:'How many elements there are in the periodic table of elements?',
+        options:['120', '118', '215', '230'],
+        answer:1
     },
     {
-      q : "What is the formula of gravity of Earth?",
-      o : [
-        "g= 35 k s-2",
-        "g= 9.81 m m-2",
-        "g= 9.81 m s-2",
-        "g= 25 m s-2"
-      ],
-      a : 2
+        q:'What is the unit of measure of the magnetic field?',
+        options:['G', 'Watt', 'Gauss', 'Candela'],
+        answer:2
     },
     {
-      q : "Which is the seventh planet from the sun?",
-      o : [
-        "Uranus",
-        "Earth",
-        "Pluto",
-        "Mars"
-      ],
-      a : 0
+      q:'What is the formula of gravity of Earth?',
+      options:['g= 9.81 km s-2', 'g= 18 m s-2', 'g= 9.81 m s-2', 'g= 9.81 m/m s-2'],
+      answer:2
     },
     {
-      q : "Which is the unit of measure of the magnetic field?",
-      o : [
-        "G",
-        "Watt",
-        "Candela",
-        "Gauss"
-      ],
-      a : 3
-    }
-]
+      q:'Which is the seventh planet from the sun?',
+      options:['Uranus', 'Venus', 'Jupiter', 'Pluto'],
+      answer:0
+    },
+    {
+      q:'What is the formula of speed?',
+      options:['speed = distance ÷ time', 'speed = distance x time', 'speed = 2distance - time', 'speed = distance ÷ time x 24'],
+      answer:0
+},
+{
+  q:'Can sound waves generate heat?',
+  options:['never', 'only during the night', 'yes', 'only during the day'],
+  answer:2
+},
+{
+  q:'What is the absolute 0?',
+  options:['−273.15 °C', '−300 °C', '−300.30 °C', '−250 °C'],
+  answer:0
+},
+{
+  q:'Sodium is a chemical element with atomic number 11. What is its symbol?',
+  options:['Nu', 'Na', 'No', 'Ni'],
+  answer:1
+},
+{
+  q:'The ... Effect makes things traveling long distances around the Earth appear to move at a curve as opposed to a straight line.',
+  options:['Newton', 'Coriolis', 'Bohr', 'Planck'],
+  answer:1
 }
+]
+
