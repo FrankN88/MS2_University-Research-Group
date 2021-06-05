07e73294373532207c1ce970b5143a5cb84f2251
FrankN88 <nardecchia.f@gmail.com>
2021-06-05 10:05:26 +0000

    Clock added, style to be finished

--- assets/js/projects.js
+++ assets/js/projects.js
@@ -190,6 +190,39 @@ function tryAgain(){
     window.location.reload();
 }
 
+// clock
+gameClock = document.getElementById('gameClock');
+
+function digitalTime(){
+	date = new Date()
+	getHours = date.getHours();
+	let ampm = (getHours >= 12) ? "PM" : "AM";
+	getHours = date.getHours() % 12 || 12;
+	getMinutes = date.getMinutes();
+	getSeconds = date.getSeconds();
+
+	if(getHours < 10){
+		getHours = "0"+getHours
+	}
+	if(getMinutes < 10){
+		getMinutes = "0"+getMinutes
+	}
+	if(getSeconds < 10){
+		getSeconds = "0"+getSeconds
+	}
+
+	ClockView = "<span>"+getHours + "</span>:<span>"+getMinutes+"</span>:<span id='second'>"+getSeconds+"</span>"+"<span id='ampm'>  "+ampm+"</span>";
+	gameClock.innerHTML= ClockView
+	setTimeout(digitalTime,1000)
+
+}
+
+digitalTime()
+
+
+
+
+
 
 
 
