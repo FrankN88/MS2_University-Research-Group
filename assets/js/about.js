a5ee59c97144cb812328c881f436fc1f7cf16a34
FrankN88 <nardecchia.f@gmail.com>
2021-06-08 15:10:18 +0000

    Indenting and commenting about JS code

--- assets/js/about.js
+++ assets/js/about.js
@@ -2,34 +2,29 @@
 const btn = document.getElementById('button');
 
 document.getElementById('form')
- .addEventListener('submit', function(event) {
-   event.preventDefault();
+    .addEventListener('submit', function(event) {
+        event.preventDefault();
 
-   btn.value = 'Sending...';
+        btn.value = 'Sending...'; // Sending in progress
 
-   const serviceID = 'default_service';
-   const templateID = 'template_l5nl898';
+        const serviceID = 'default_service';
+        const templateID = 'template_l5nl898';
 
-   emailjs.sendForm(serviceID, templateID, this)
-    .then(() => {
-      btn.value = 'Send Email';
-      alert('Sent!');
-    }, (err) => {
-      btn.value = 'Send Email';
-      alert(JSON.stringify(err));
+        emailjs.sendForm(serviceID, templateID, this)
+            .then(() => {
+                btn.value = 'Send Email';
+                alert('Sent!'); // Success sending email
+            }, (err) => {
+                btn.value = 'Send Email';
+                alert(JSON.stringify(err)); // Error
+            });
+        this.reset() // Resetting fields after sending
+
     });
-    this.reset()
-
-});
-
-
-
-
 
 // Text area that autogrows based on its content
-
-document.getElementById("field").addEventListener("input", function(){
-  this.style.height = 'inherit';
-  let height = this.scrollHeight;
-  this.style.height = height + "px";
+document.getElementById("field").addEventListener("input", function() {
+    this.style.height = 'inherit';
+    let height = this.scrollHeight;
+    this.style.height = height + "px";
 })
